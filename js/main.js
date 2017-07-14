595041c85b326622d9f94936768fc82e2531550e
Minho Lee <minho.lee.93@hotmail.com>
2017-07-14 14:29:19 -0400

    adding some js/css files so that it doesn't take long to load. Adding carousel captions, and allowing the menu to collapse when clicked elsewhere (for mobile)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-$('body nav ul li').on('click', 'a', function() {
+$('body nav ul li').on('click', 'a', function(e) {
 	if ($(this).attr('href') === '#whoWeAre') {
 		scrollTo('section1');
 	} else if ($(this).attr('href') === '#whatWeDo') {
@@ -11,13 +11,27 @@ $('body nav ul li').on('click', 'a', function() {
 	} else {
 		scrollTo('section5');
 	}
+	e.preventDefault();
 });
 
 
 
 
 $(document).ready(function() {
-	
+	$(document).click(function(event) {
+		var clickover = $(event.target);
+		var _opened = $('.navbar-collapse').hasClass('in');
+
+		var navMain = $(".navbar-collapse");
+
+		navMain.on('click', 'a:not([data-toggle])', null, function() {
+			navMain.collapse('hide');
+		});
+		
+		if (!clickover.hasClass('navbar-collapse') && _opened) {
+			navMain.collapse('hide');
+		}
+	});//document.click
 });
 
 //scrolls to the given id's position
