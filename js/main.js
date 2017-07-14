e4c457c82da6128763a3973b0c0f80019df6a03b
Minho Lee <minho.lee.93@hotmail.com>
2017-07-14 16:08:42 -0400

    update carousel caption and positioning

--- js/main.js
+++ js/main.js
@@ -1,23 +1,10 @@
-
-$('body nav ul li').on('click', 'a', function(e) {
-	if ($(this).attr('href') === '#whoWeAre') {
-		scrollTo('section1');
-	} else if ($(this).attr('href') === '#whatWeDo') {
-		scrollTo('section2');
-	} else if ($(this).attr('href') === '#whyChooseUs') {
-		scrollTo('section3');
-	} else if ($(this).attr('href') === '#ourteam') {
-		scrollTo('section4');
-	} else {
-		scrollTo('section5');
-	}
-	e.preventDefault();
-});
-
-
-
-
 $(document).ready(function() {
+
+	//when refreshed, scroll back to the top automatically
+	scrollTo('wrapper-body');
+   $('body').scrollTop('0'); //For Chrome, Safari and Opera
+   document.documentElement.scrollTop = 0; // For IE and Firefox
+
 	$(document).click(function(event) {
 		var clickover = $(event.target);
 		var _opened = $('.navbar-collapse').hasClass('in');
@@ -31,12 +18,23 @@ $(document).ready(function() {
 		if (!clickover.hasClass('navbar-collapse') && _opened) {
 			navMain.collapse('hide');
 		}
+	if (clickover.attr('href') === '#whoWeAre') {
+			scrollTo('section1');
+		} else if (clickover.attr('href') === '#whatWeDo') {
+			scrollTo('section2');
+		} else if (clickover.attr('href') === '#whyChooseUs') {
+			scrollTo('section3');
+		} else if (clickover.attr('href') === '#ourteam') {
+			scrollTo('section4');
+		} else if (clickover.attr('href') === '#contactUs') {
+			scrollTo('section5');
+		}
 	});//document.click
-});
+});//document.ready
 
 //scrolls to the given id's position
 var scrollTo = function(id) {
    $('html, body').animate({
                   scrollTop: $("#" + id).offset().top-50
-               }, 2000);
+               }, 1500);
 }
